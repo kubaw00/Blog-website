f6d4ec977f288e099c5f116818f9e4dcb65bf7a0
Jakub Widurek <jakubwidurek@gmail.com>
2021-10-27 00:10:30 +0200

    render dynamic post pages & new markdown file

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,21 +2,14 @@ import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import ReactMarkdown from 'react-markdown';
 
-const DUMMY_POST = {
-  title: 'Getting Started With NextJs',
-  slug: 'getting-started-with-nextjs',
-  image: 'getting-started-nextjs.png',
-  content: '# This is a first post',
-  date: '2022-02-10',
-};
-
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <PostHeader title={post.title} image={imagePath} />
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
