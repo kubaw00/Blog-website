6cf3f7ce17522df0ad4f440d19fbc842676f4f46
Jakub Widurek <jakubwidurek@gmail.com>
2021-10-27 22:07:16 +0200

    add portal to notification component

--- pages/_document.js
+++ pages/_document.js
@@ -1,15 +1,16 @@
+// eslint-disable-next-line @next/next/no-document-import-in-page
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
-          <body>
-            <Main />
-            <NextScript />
-          </body>
-        </Head>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <div id="notifications"></div>
+        </body>
       </Html>
     );
   }
