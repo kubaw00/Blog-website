36f74793ad44c7deb476ba868c785efc479c4e3d
Jakub Widurek <jakubwidurek@gmail.com>
2021-10-27 17:33:08 +0200

    store data in mongodb database

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,4 +1,6 @@
-function handler(req, res) {
+import { MongoClient } from 'mongodb';
+
+async function handler(req, res) {
   if (req.method === 'POST') {
     console.log(req.body);
     const { email, name, message } = req.body;
@@ -21,7 +23,27 @@ function handler(req, res) {
       name,
       message,
     };
-    console.log(newMessage);
+
+    let client;
+    try {
+      client = await MongoClient.connect(
+        'mongodb+srv://kuba941:Tqp2cjqE0XkGHgf2@cluster0.vzzyb.mongodb.net/my-site?retryWrites=true&w=majority'
+      );
+    } catch (error) {
+      res.status(500).json({ message: 'Could not connect to database.' });
+    }
+
+    const db = client.db();
+    try {
+      const result = await db.collection('messages').insertOne(newMessage);
+      console.log(result);
+      newMessage.id = result.insertedId;
+    } catch (error) {
+      client.close();
+      res.status(500).json({ message: 'Storing messgae failed' });
+      return;
+    }
+    client.close();
 
     res.status(201).json({ message: newMessage });
   }
