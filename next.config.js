64e6fff018e98e79d62ab356584a7f760c313ce6
Jakub Widurek <jakubwidurek@gmail.com>
2021-10-28 17:10:17 +0200

    changing environmental variables

--- next.config.js
+++ next.config.js
@@ -1,24 +1,3 @@
-const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
-
-module.exports = (phase) => {
-  if (PHASE_DEVELOPMENT_SERVER) {
-    return {
-      env: {
-        mongodb_username: 'kuba941',
-        mongodb_password: 'Tqp2cjqE0XkGHgf2',
-        mongodb_clustername: 'cluster0',
-        mongodb_database: 'my-site',
-      },
-      reactStrictMode: true,
-    };
-  }
-  return {
-    env: {
-      mongodb_username: 'kuba941',
-      mongodb_password: 'Tqp2cjqE0XkGHgf2',
-      mongodb_clustername: 'cluster0',
-      mongodb_database: 'my-site',
-    },
-    reactStrictMode: true,
-  };
+module.exports = {
+  reactStrictMode: true,
 };
