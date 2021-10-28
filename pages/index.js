300d996dab5561b33b718c119ea005183bfc7b2f
Jakub Widurek <jakubwidurek@gmail.com>
2021-10-28 17:49:52 +0200

    change hero import

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Hero from '../components/home-page/Hero';
+import Hero from '../components/home-page/hero';
 import FeaturedPosts from '../components/home-page/featured-posts';
 import { getFeaturedPosts } from '../lib/post-util';
 import Head from 'next/head';
